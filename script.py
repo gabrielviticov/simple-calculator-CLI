eb09956a02dc2834b63ce1488b2e4a514cd3d828
gabrielviticov <gabrielviticov.ctt@gmail.com>
2021-05-05 08:00:36 -0300

    Foi criado o arquivo da aplicação - Script 1.0.
    O arquivo foi criado e já está sendo salvo como a primeira versão do aplicativo.

---
+++ script.py
@@ -0,0 +1,13 @@
+# Versão da Aplicação - versão 1.0
+    #Criação de menus
+
+print('_'*25)
+print('SIMPLE CALCULATOR ON CLI')
+print('_'*25)
+print('''
+[1] SOMA 
+[2] SUBTRAÇÃO
+[3] MULTIPLICAÇÃO
+[4] DIVISÃO
+''')
+
