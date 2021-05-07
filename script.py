05323f97729d1745a85aa1a0f65ce328189e15fb
gabrielviticov <gabrielviticov.ctt@gmail.com>
2021-05-07 15:45:53 -0300

    Versão da aplicação - v3.0
    Foi implementado duas seções de interatividade no laço de repetição.

--- script.py
+++ script.py
@@ -1,18 +1,27 @@
-# Versão da Aplicação - versão 2.0
-    #Aprimorando Menus
+# Versão da Aplicação - versão 3.0
+    #Iniciando interatividade
+
+num1 = int(input('Primeiro valor: '))
+num2 = int(input('Segundo valor: '))
 
 print('-'*25)
 print('SIMPLE CALCULATOR ON CLI')
 print('_'*25)
+
 while True:
     print('''
     [1] SOMA 
     [2] SUBTRAÇÃO
     [3] MULTIPLICAÇÃO
     [4] DIVISÃO
+    [5] NOVOS VALORES
     [0] SAIR
     ''')
     escolha = int(input('Informe a escolha desejada: '))
+    print('-'*25)
     if escolha == 0:
         break
-print('OBRIGADO POR TESTAR O BETA! VOLTE SEMPRE!')
+    elif escolha == 5:
+        num1 = int(input('Primeiro valor: '))
+        num2 = int(input('Segundo valor: '))
+print('OBRIGADO POR TESTAR A APLICAÇÃO! VOLTE SEMPRE!')
